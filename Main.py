a1ffe0b239e93c138ce29f67c7e0b774b6f2ace1
coderhanzla@gmail.com <coderhanzla@gmail.com>
2025-02-21 21:23:28 +0530

    Swap2Numbers

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 def swap(a , b):
-      a = a^b
-      b = a^b
-      a = a^b
-      print("After swapping: a =",a,"and b =",b)
+    #   a = a^b
+    #   b = a^b
+    #   a = a^b
+
+    a = (a&b)+(a|b)
+    b = a+(~b)+1
+    a = a+(~b)+1
+    print("After swapping: a =",a,"and b =",b)
 
 
 
